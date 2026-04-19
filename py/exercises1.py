e97a7ba8c133a3ced09501eabe3dc4efa5e9b364
lekaa <lekaakassab7@gmail.com>
2026-04-19 19:14:27 +0200

    Add files via upload

---
+++ py/exercises1.py
@@ -0,0 +1,18 @@
+#1
+age = 27
+print(age)
+
+#2
+name = "lolo"
+print("hello" + name)
+
+#3
+fruit = ["banana","apple","grape","kiwi ","lemon"]
+print(fruit[1])
+
+#4
+print(int('123') + 10)
+
+#5
+from math import sqrt as num
+print(num(16))
